64ac1dd7fc60e9448526afb34ca3a513aa272baa
Omid Mafakher <omid.mafakher@outlook.com>
2023-03-16 19:46:28 +0100

    Try to add fullscreen mode for android

--- src/Example/ShellExample/ShellExample.Android/MainActivity.cs
+++ src/Example/ShellExample/ShellExample.Android/MainActivity.cs
@@ -1,11 +1,21 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using AndroidX.Core.View;
 using Avalonia.Android;
 
 namespace ShellExample.Android;
 
-[Activity(Label = "ShellExample.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon",
+[Activity(Label = "ShellExample.Android", Theme = "@style/MyTheme.Main", Icon = "@drawable/icon",
+
 	LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
 public class MainActivity : AvaloniaMainActivity
 {
+	protected override void OnCreate(Bundle savedInstanceState)
+	{
+		base.OnCreate(savedInstanceState);
+		AppCompatDelegate.DefaultNightMode = AppCompatDelegate.ModeNightYes;
+		WindowCompat.SetDecorFitsSystemWindows(Window, false);
+	}
 }
