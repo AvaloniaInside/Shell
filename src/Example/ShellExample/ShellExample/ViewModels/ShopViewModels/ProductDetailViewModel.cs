022e2b8c9b45d72248086a28683214be71476235
Omid Mafakher <Omid@mafakher.name>
2023-08-26 14:10:48 +0200

    Add product details

---
+++ src/Example/ShellExample/ShellExample/ViewModels/ShopViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ShellExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellExample.ViewModels.ShopViewModels;
+public class ProductDetailViewModel : ViewModelBase
+{
+    public ProductDto Product { get; set; }
+
+	public ProductDetailViewModel(ProductDto product)
+	{
+        Product = product;
+    }
+}
