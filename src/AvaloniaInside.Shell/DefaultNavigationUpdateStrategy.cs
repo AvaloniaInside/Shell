82618a0659dbff00dc4956c82810a3c9fcdfb3fb
zyknow <1047606598@qq.com>
2023-09-22 14:19:13 +0800

    chore: format code

--- src/AvaloniaInside.Shell/DefaultNavigationUpdateStrategy.cs
+++ src/AvaloniaInside.Shell/DefaultNavigationUpdateStrategy.cs
@@ -10,110 +10,109 @@ namespace AvaloniaInside.Shell;
 
 public class DefaultNavigationUpdateStrategy : INavigationUpdateStrategy
 {
-    private readonly IPresenterProvider _presenterProvider;
+	private readonly IPresenterProvider _presenterProvider;
 
-    public DefaultNavigationUpdateStrategy(IPresenterProvider presenterProvider)
-    {
-        _presenterProvider = presenterProvider;
-    }
+	public DefaultNavigationUpdateStrategy(IPresenterProvider presenterProvider)
+	{
+		_presenterProvider = presenterProvider;
+	}
 
-    public event EventHandler<HostItemChangeEventArgs>? HostItemChanged;
+	public event EventHandler<HostItemChangeEventArgs>? HostItemChanged;
 
-    public async Task UpdateChangesAsync(
-        ShellView shellView,
-        NavigationStackChanges changes,
-        List<object> newInstances,
-        NavigateType navigateType,
-        object? argument,
-        bool hasArgument,
-        CancellationToken cancellationToken)
-    {
-        var isSame = changes.Previous == changes.Front;
+	public async Task UpdateChangesAsync(
+		ShellView shellView,
+		NavigationStackChanges changes,
+		List<object> newInstances,
+		NavigateType navigateType,
+		object? argument,
+		bool hasArgument,
+		CancellationToken cancellationToken)
+	{
+		var isSame = changes.Previous == changes.Front;
 
-        foreach (var instance in newInstances)
-        {
-            if (instance is INavigationLifecycle navigationLifecycle)
-                await navigationLifecycle.InitialiseAsync(cancellationToken);
+		foreach (var instance in newInstances)
+		{
+			if (instance is INavigationLifecycle navigationLifecycle)
+				await navigationLifecycle.InitialiseAsync(cancellationToken);
 
-            SubscribeForUpdateIfNeeded(instance);
-        }
+			SubscribeForUpdateIfNeeded(instance);
+		}
 
-        if (changes.Previous?.Instance is INavigationLifecycle oldInstanceLifecycle && !isSame)
-            await oldInstanceLifecycle.DisappearAsync(cancellationToken);
+		if (changes.Previous?.Instance is INavigationLifecycle oldInstanceLifecycle && !isSame)
+			await oldInstanceLifecycle.DisappearAsync(cancellationToken);
 
-        if (changes.Removed != null)
-            await InvokeRemoveAsync(shellView, changes.Removed, changes.Previous, navigateType, cancellationToken);
+		if (changes.Removed != null)
+			await InvokeRemoveAsync(shellView, changes.Removed, changes.Previous, navigateType, cancellationToken);
 
-        if (changes.Front?.Instance is INavigationLifecycle newInstanceLifecycle)
-        {
-            if (!isSame)
-                await newInstanceLifecycle.AppearAsync(cancellationToken);
+		if (changes.Front?.Instance is INavigationLifecycle newInstanceLifecycle)
+		{
+			if (!isSame)
+				await newInstanceLifecycle.AppearAsync(cancellationToken);
 
-            if (hasArgument)
-                await newInstanceLifecycle.ArgumentAsync(argument, cancellationToken);
+			if (hasArgument)
+				await newInstanceLifecycle.ArgumentAsync(argument, cancellationToken);
+			
+			var hasQuery = !string.IsNullOrWhiteSpace(changes.Front.Uri.Query);
+			if (hasQuery && (changes.Front.Instance as Control)?.DataContext is IQueryAttributable queryAttributable)
+			{
+				var query = changes.Front.Uri.Query;
 
-            var hasQuery = !string.IsNullOrWhiteSpace(changes.Front.Uri.Query);
-            if (hasQuery && (changes.Front.Instance as Control)?.DataContext is IQueryAttributable queryAttributable)
-            {
-                var query = changes.Front.Uri.Query;
+				var queryDict = query
+					.TrimStart('?')
+					.Split('&')
+					.Select(x => x.Split('='))
+					.Where(x => x.Length == 2)
+					.ToDictionary(x => x[0], x => (object) x[1]);
 
-                var queryDict = query
-                    .TrimStart('?')
-                    .Split('&')
-                    .Select(x => x.Split('='))
-                    .Where(x => x.Length == 2)
-                    .ToDictionary(x => x[0], x => (object) x[1]);
+				queryAttributable.ApplyQueryAttributes(queryDict);
+			}
+		}
 
-                queryAttributable.ApplyQueryAttributes(queryDict);
-            }
-        }
+		if (!isSame && changes.Front != null)
+			await _presenterProvider.For(navigateType).PresentAsync(shellView, changes.Front, navigateType, cancellationToken);
+	}
 
-        if (!isSame && changes.Front != null)
-            await _presenterProvider.For(navigateType)
-                .PresentAsync(shellView, changes.Front, navigateType, cancellationToken);
-    }
-
-    private async Task InvokeRemoveAsync(ShellView shellView,
+	private async Task InvokeRemoveAsync(ShellView shellView,
         IList<NavigationChain> removed,
         NavigationChain? previous,
         NavigateType navigateType,
         CancellationToken cancellationToken)
-    {
-        var presenter = _presenterProvider.Remove();
-        foreach (var chain in removed)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-            if (previous == chain)
-                await _presenterProvider.Remove().PresentAsync(shellView, previous, navigateType, cancellationToken);
-            else
-                await presenter.PresentAsync(shellView, chain, navigateType, cancellationToken);
+	{
+		var presenter = _presenterProvider.Remove();
+		foreach (var chain in removed)
+		{
+			cancellationToken.ThrowIfCancellationRequested();
+			if (previous == chain)
+				await _presenterProvider.Remove().PresentAsync(shellView, previous, navigateType, cancellationToken);
+			else
+				await presenter.PresentAsync(shellView, chain, navigateType, cancellationToken);
 
-            if (chain.Instance is INavigationLifecycle lifecycle)
-                await lifecycle.TerminateAsync(cancellationToken);
+			if (chain.Instance is INavigationLifecycle lifecycle)
+				await lifecycle.TerminateAsync(cancellationToken);
 
-            UnSubscribeForUpdateIfNeeded(chain.Instance);
-        }
-    }
+			UnSubscribeForUpdateIfNeeded(chain.Instance);
+		}
+	}
 
-    private void SubscribeForUpdateIfNeeded(object? instance)
-    {
-        if (instance is not SelectingItemsControl selectingItemsControl) return;
-        selectingItemsControl.SelectionChanged += SelectingItemsControlOnSelectionChanged;
-    }
+	private void SubscribeForUpdateIfNeeded(object? instance)
+	{
+		if (instance is not SelectingItemsControl selectingItemsControl) return;
+		selectingItemsControl.SelectionChanged += SelectingItemsControlOnSelectionChanged;
+	}
 
-    private void UnSubscribeForUpdateIfNeeded(object instance)
-    {
-        if (instance is not SelectingItemsControl selectingItemsControl) return;
-        selectingItemsControl.SelectionChanged -= SelectingItemsControlOnSelectionChanged;
-    }
+	private void UnSubscribeForUpdateIfNeeded(object instance)
+	{
+		if (instance is not SelectingItemsControl selectingItemsControl) return;
+		selectingItemsControl.SelectionChanged -= SelectingItemsControlOnSelectionChanged;
+	}
 
-    private void SelectingItemsControlOnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-    {
-        if (e.AddedItems?.Count > 0 && e.AddedItems[0] is NavigationChain chain)
-        {
-            HostItemChanged?.Invoke(this, new HostItemChangeEventArgs(
-                e.RemovedItems?.Count > 0 ? e.RemovedItems[0] as NavigationChain : null,
-                chain));
-        }
-    }
+	private void SelectingItemsControlOnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		if (e.AddedItems?.Count > 0 && e.AddedItems[0] is NavigationChain chain)
+		{
+			HostItemChanged?.Invoke(this, new HostItemChangeEventArgs(
+				e.RemovedItems?.Count > 0 ? e.RemovedItems[0] as NavigationChain : null,
+				chain));
+		}
+	}
 }
