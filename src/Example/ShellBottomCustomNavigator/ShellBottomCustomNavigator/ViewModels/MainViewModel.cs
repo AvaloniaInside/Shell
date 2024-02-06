7198f1bda391b48652e26594e5071398f70374bf
lukewire <lukewire129@gmail.com>
2024-02-06 09:41:01 +0900

    Missing file

---
+++ src/Example/ShellBottomCustomNavigator/ShellBottomCustomNavigator/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Animation;
+using AvaloniaInside.Shell.Platform;
+using ReactiveUI;
+
+namespace ShellBottomCustomNavigator.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+    private IPageTransition _currentTransition = PlatformSetup.TransitionForPage;
+    public IPageTransition CurrentTransition
+    {
+        get => _currentTransition;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _currentTransition, value);
+        }
+    }
+}
