44f6ae7a7f7a33d8757428d5b422e1257f76c1d5
Daniel Meza <daniel.meza2011@gmail.com>
2024-06-09 11:41:15 -0500

    Add ability to provide a custom INavigationViewLocator so other OIC c… (#32)
    * Add ability to provide a custom INavigationViewLocator so other OIC containers can be used to create views.
    
    * Invoke viewFactoryFactory inside INavigator factory
    
    ---------
    
    Co-authored-by: Daniel Meza <daniel.meza-contractor@procore.com>

--- src/AvaloniaInside.Shell/AppBuilderExtensions.cs
+++ src/AvaloniaInside.Shell/AppBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using AvaloniaInside.Shell.Presenters;
 using Splat;
@@ -6,7 +7,7 @@ namespace AvaloniaInside.Shell;
 
 public static class AppBuilderExtensions
 {
-	public static AppBuilder UseShell(this AppBuilder builder) =>
+	public static AppBuilder UseShell(this AppBuilder builder, Func<INavigationViewLocator>? viewLocatorFactory = null) =>
 		builder.AfterPlatformServicesSetup(_ =>
 		{
 			if (Locator.CurrentMutable is null)
@@ -16,18 +17,37 @@ public static class AppBuilderExtensions
 
 			Locator.CurrentMutable.Register<INavigationRegistrar, NavigationRegistrar>();
 			Locator.CurrentMutable.Register<IPresenterProvider, PresenterProvider>();
-			Locator.CurrentMutable.Register<INavigationViewLocator, DefaultNavigationViewLocator>();
+
+			if (viewLocatorFactory is null)
+			{
+				Locator.CurrentMutable.Register<INavigationViewLocator, DefaultNavigationViewLocator>();
+			}
+			
 			Locator.CurrentMutable.Register<INavigationUpdateStrategy>(() =>
 				new DefaultNavigationUpdateStrategy(Locator.Current.GetService<IPresenterProvider>()!));
 
 			Locator.CurrentMutable.Register<INavigator>(() =>
 			{
+				var viewLocator = viewLocatorFactory != null ? viewLocatorFactory.Invoke() : Locator.Current.GetService<INavigationViewLocator>()!;
 				var registrar = Locator.Current.GetService<INavigationRegistrar>()!;
 				return new Navigator(
 					registrar,
 					new RelativeNavigateStrategy(registrar),
 					Locator.Current.GetService<INavigationUpdateStrategy>()!,
-					Locator.Current.GetService<INavigationViewLocator>()!);
+					viewLocator
+					);
 			});
 		});
+
+	public static AppBuilder UseShell(this AppBuilder builder, Func<NavigationNode, object> viewFactory)
+		=> builder.UseShell(() => new DelegateNavigationViewLocator(viewFactory));
+
+	private class DelegateNavigationViewLocator(Func<NavigationNode, object> viewFactory)
+		    : INavigationViewLocator
+	    {
+		    public object GetView(NavigationNode navigationItem)
+		    {
+			    return viewFactory(navigationItem);
+		    }
+	    }
 }
