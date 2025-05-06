0c5bf5efeeb95e3fd1d8be390d405fe25f47a402
Omid Mafakher <omid.mafakher@outlook.com>
2025-05-06 14:50:53 +0200

    Create renovate-config.js

---
+++ .github/configs /renovate-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  platform: 'github',
+  branchPrefix: 'renovate/',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  automerge: false,
+  onboarding: false,
+  platform: 'github',
+  forkProcessing: 'enabled',
+  dryRun: 'full',
+  allowPostUpgradeCommandTemplating: true,
+  allowedPostUpgradeCommands: [".*"]
+};
