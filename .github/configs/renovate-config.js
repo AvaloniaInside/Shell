5d86c1d2f70f692cf6dda0d323662254d123ca1b
Omid Mafakher <omid.mafakher@outlook.com>
2025-05-08 10:32:06 +0200

    Update renovate-config.js

--- .github/configs/renovate-config.js
+++ .github/configs/renovate-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "extends": ["config:base"],
+  "extends": ["config:recommended"],
+  "repositories": ["AvaloniaInside/Shell"],
   "platform": "github",
   "branchPrefix": "renovate/",
   "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
   "automerge": false,
   "forkProcessing": "enabled",
   "allowedCommands": [".*"],
-  "autodiscover": true,
   "prCreation": "immediate"
 };
